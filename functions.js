03821840c793590f391c8ee5a348a13dbd085d8c
LeslieGaribay <131633753+LeslieGaribay@users.noreply.github.com>
2023-08-03 17:52:27 -0600

    Función getRouteType, tests válidos de funciones 1, 2 y 3

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { isAbsolute, resolve } = require('path');
 
-const fileRoute = '.\\Example';
+const fileRoute = 'C:\\Users\\Leslie\\Documents\\Laboratoria\\DEV008-md-links\\Example\\file-1.md';
 
 function isPathValid(path, callback) {
     fs.access(path, (error) => {
@@ -13,25 +13,68 @@ function isPathValid(path, callback) {
     });
 }
 
+// isPathValid(fileRoute, (exists) => {
+//     if (exists) {
+//         console.log('The file path exists.');
+//     } else {
+//         console.log('The file path does not exist.');
+//     }
+// });
+
 function makePathAbsolute(path) {
     if (isAbsolute(path)) {
         return path;
     } else {
         return resolve(path);
     }
+};
+
+// console.log(makePathAbsolute(fileRoute));
+
+// function isDirectory(fileRoute) {
+//     if (fs.access(fileRoute).isDirectory()) {
+//         const files = fs.access(path);
+//         const mdFiles = files.filter(file => path.extname(file) === 'md');
+//         if (mdFiles.length > 0) {
+//             console.log(`Files with extension .md in the path ${fileRoute}:`);
+//             mdFiles.forEach(file => console.log(file));
+//         } else {
+//             console.log(`No files with .md extension were found in the path ${fileRoute}`)
+//         }
+//         return true;
+
+//     } else {
+//         console.log('The path provided is a file, not a directory');
+//     }
+// }
+// console.log(isDirectory(fileRoute));
+
+function getRouteType(fileRoute, callback) {
+  fs.stat(fileRoute, (error, stats) => {
+    if (error) {
+      return callback(`Error getting route type: ${error.message}`);
+    }
+
+    if (stats.isFile()) {
+      return callback(null, 'file');
+    } else if (stats.isDirectory()) {
+      return callback(null, 'directory');
+    } else {
+      return callback(null, 'unknown');
+    }
+  });
 }
 
-isPathValid(fileRoute, (exists) => {
-    if (exists) {
-        console.log('The file path exists.');
-    } else {
-        console.log('The file path does not exist.');
-    }
-});
-
-console.log(makePathAbsolute(fileRoute));
+// getRouteType(fileRoute, (error, routeType) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log(`The path ${fileRoute} is a ${routeType}.`);
+//   }
+// });
 
 module.exports = {
     isPathValid,
     makePathAbsolute,
+    getRouteType
 }
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,81 @@
-const mdLinks = require('../');
+const { isPathValid } = require('../functions.js');
 
+const fakeAbsoluteRoute = 'C:\\Users\\Leslie\\Documents\\Laboratoria\\DEV008-md-links\\Example';
+const fakeRelativeRoute = '..\\Example';
+const fakeInvalidRoute = 'C:\\Users\\Leslie\\Documents\\Labororia\\DElinks\\Example';
 
-describe('mdLinks', () => {
+function tick() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 0);
+  });
+}
 
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('test para isPathValid', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.resetModules();
   });
 
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.resetModules();
+  });
+
+  it('debería pasar una ruta válida', async () => {
+    const mockCallback = jest.fn();
+    isPathValid(fakeAbsoluteRoute, mockCallback);
+    await tick();
+    await tick();
+    await tick();
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+    expect(mockCallback).toHaveBeenCalledWith(true);
+  });
+
+  it('debería mostrar error si la ruta es inválida', async () => {
+    const mockCallback = jest.fn();
+    isPathValid(fakeInvalidRoute, mockCallback);
+    await tick();
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+    expect(mockCallback).toHaveBeenCalledWith(false);
+  });
 });
+
+describe('test para makePathAbsolute', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.resetModules();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.resetModules();
+  });
+  
+  it('debería convertir una ruta relativa a una absoluta', async () => {
+    const fakeAbsoluteRoute = 'C:\\Users\\Leslie\\Documents\\Laboratoria\\DEV008-md-links\\Example';
+    const mockIsAbsolute = jest.fn().mockImplementation(() => false);
+    const mockResolve = jest.fn().mockImplementation(() => fakeAbsoluteRoute);
+    jest.mock('path', () => ({
+      isAbsolute: mockIsAbsolute,
+      resolve: mockResolve
+    }));
+    const { makePathAbsolute } = require('../functions.js');
+    const result = makePathAbsolute(fakeRelativeRoute);
+    await tick();
+    expect(result).toBe(fakeAbsoluteRoute);
+    expect(mockIsAbsolute).toHaveBeenCalledTimes(1);
+    expect(mockResolve).toHaveBeenCalledTimes(1);
+    expect(mockResolve).toHaveBeenCalledWith(fakeRelativeRoute);
+  });
+});
+
+// const mdLinks = require('../');
+
+
+// describe('mdLinks', () => {
+
+//   it('should...', () => {
+//     console.log('FIX ME!');
+//   });
+
+// });
