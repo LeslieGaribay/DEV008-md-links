127339e78e5467cefac9d5cc37365ad524a61fb2
LeslieGaribay <131633753+LeslieGaribay@users.noreply.github.com>
2023-08-07 14:41:28 -0600

    Función getMdFilesInDirectory muestra archivos .md en carpeta

--- functions.js
+++ functions.js
@@ -1,53 +1,36 @@
 const fs = require('fs');
 const { isAbsolute, resolve } = require('path');
+const path = require('path');
 
-const fileRoute = 'C:\\Users\\Leslie\\Documents\\Laboratoria\\DEV008-md-links\\Example\\file-1.md';
+const fileRoute = './Example';
 
 function isPathValid(path, callback) {
-    fs.access(path, (error) => {
-        if (error) {
-            callback(false);
-        } else {
-            callback(true);
-        }
-    });
+  fs.access(path, (error) => {
+    if (error) {
+      callback(false);
+    } else {
+      callback(true);
+    }
+  });
 }
 
-// isPathValid(fileRoute, (exists) => {
-//     if (exists) {
-//         console.log('The file path exists.');
-//     } else {
-//         console.log('The file path does not exist.');
-//     }
-// });
+isPathValid(fileRoute, (exists) => {
+  if (exists) {
+    console.log('The file path exists.');
+  } else {
+    console.log('The file path does not exist.');
+  }
+});
 
 function makePathAbsolute(path) {
-    if (isAbsolute(path)) {
-        return path;
-    } else {
-        return resolve(path);
-    }
+  if (isAbsolute(path)) {
+    return path;
+  } else {
+    return resolve(path);
+  }
 };
 
-// console.log(makePathAbsolute(fileRoute));
-
-// function isDirectory(fileRoute) {
-//     if (fs.access(fileRoute).isDirectory()) {
-//         const files = fs.access(path);
-//         const mdFiles = files.filter(file => path.extname(file) === 'md');
-//         if (mdFiles.length > 0) {
-//             console.log(`Files with extension .md in the path ${fileRoute}:`);
-//             mdFiles.forEach(file => console.log(file));
-//         } else {
-//             console.log(`No files with .md extension were found in the path ${fileRoute}`)
-//         }
-//         return true;
-
-//     } else {
-//         console.log('The path provided is a file, not a directory');
-//     }
-// }
-// console.log(isDirectory(fileRoute));
+console.log(makePathAbsolute(fileRoute));
 
 function getRouteType(fileRoute, callback) {
   fs.stat(fileRoute, (error, stats) => {
@@ -65,16 +48,53 @@ function getRouteType(fileRoute, callback) {
   });
 }
 
-// getRouteType(fileRoute, (error, routeType) => {
-//   if (error) {
-//     console.error(error);
+getRouteType(fileRoute, (error, routeType) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`The path ${fileRoute} is a ${routeType}.`);
+  }
+});
+
+function getMdFilesInDirectory(fileRoute, callback) {
+  fs.readdir(fileRoute, (error, files) => {
+    if (error) {
+      return callback(new Error(`Error getting the .md files in the directory: ${error.message}`))
+    }
+    const mdFiles = files.filter(file => path.extname(file) === '.md');
+    return callback(null, mdFiles);
+  });
+}
+
+getMdFilesInDirectory(fileRoute, (error, mdFiles) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    console.log(`Files with extension .md in the path ${fileRoute}:`);
+    console.log(mdFiles)
+  }
+});
+
+//   if(fs.access(fileRoute).isDirectory()) {
+//     const files = fs.access(path);
+//     const mdFiles = files.filter(file => path.extname(file) === 'md');
+//     if (mdFiles.length > 0) {
+//       console.log(`Files with extension .md in the path ${fileRoute}:`);
+//       mdFiles.forEach(file => console.log(file));
+//     } else {
+//       console.log(`No files with .md extension were found in the path ${fileRoute}`)
+//     }
+//     return true;
+
 //   } else {
-//     console.log(`The path ${fileRoute} is a ${routeType}.`);
+//     console.log('The path provided is a file, not a directory');
 //   }
-// });
+// }
+// console.log(getFiles(fileRoute));
 
 module.exports = {
-    isPathValid,
-    makePathAbsolute,
-    getRouteType
+  isPathValid,
+  makePathAbsolute,
+  getRouteType,
+  getMdFilesInDirectory
 }
